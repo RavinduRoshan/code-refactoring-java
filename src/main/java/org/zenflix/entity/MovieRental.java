e4fd0118f05faa6f88b46a6e29b09d5277be2948
unknown <ravinduroshan.rr@gmail.com>
2024-05-29 21:38:09 +0200

    use movie id instead movie object

--- src/main/java/org/zenflix/entity/MovieRental.java
+++ src/main/java/org/zenflix/entity/MovieRental.java
@@ -3,8 +3,8 @@ package org.zenflix.entity;
 /**
  * Represents a rental of a movie, including the movie itself and the number of days it is rented for.
  *
- * @param movie the movie that is rented
- * @param days  the number of days the movie is rented for
+ * @param movieId the movieId that is rented
+ * @param days    the number of days the movie is rented for
  */
-public record MovieRental (Movie movie, int days) {
+public record MovieRental(String movieId, int days) {
 }
