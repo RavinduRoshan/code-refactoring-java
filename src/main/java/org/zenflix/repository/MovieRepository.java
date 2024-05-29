02c89429c018bcb3ae38520ca73bb3b8a34cbf98
unknown <ravinduroshan.rr@gmail.com>
2024-05-29 10:29:40 +0200

    add javadoc for repository classes

--- src/main/java/org/zenflix/repository/MovieRepository.java
+++ src/main/java/org/zenflix/repository/MovieRepository.java
@@ -4,8 +4,23 @@ import org.zenflix.entity.Movie;
 
 import java.util.Map;
 
+/**
+ * Repository interface for accessing movie data.
+ */
 public interface MovieRepository {
+
+    /**
+     * Retrieves all movies from the repository.
+     *
+     * @return a map of movie IDs to movie objects
+     */
     Map<String, Movie> getAllMovies();
 
+    /**
+     * Finds a movie by its ID.
+     *
+     * @param movieId the ID of the movie to find
+     * @return the movie with the given ID, or {@code null} if no such movie exists
+     */
     Movie findById(String movieId);
 }
--- src/main/java/org/zenflix/repository/impl/MovieRepositoryImpl.java
+++ src/main/java/org/zenflix/repository/impl/MovieRepositoryImpl.java
@@ -7,23 +7,47 @@ import org.zenflix.repository.MovieRepository;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Implementation of the {@link MovieRepository} interface that uses an in-memory data structure to store movies.
+ */
 public class MovieRepositoryImpl implements MovieRepository {
     private final Map<String, Movie> movies = new HashMap<>();
 
+    /**
+     * Constructs a new {@code MovieRepositoryImpl} and populates it with a default set of movies.
+     */
     public MovieRepositoryImpl() {
         populateMovies();
     }
 
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Retrieves all movies from the repository.
+     *
+     * @return a map of movie IDs to movie objects
+     */
     @Override
     public Map<String, Movie> getAllMovies() {
         return this.movies;
     }
 
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Finds a movie by its ID.
+     *
+     * @param movieId the ID of the movie to find
+     * @return the movie with the given ID, or {@code null} if no such movie exists
+     */
     @Override
     public Movie findById(String movieId) {
         return movies.get(movieId);
     }
 
+    /**
+     * Populates the repository with a default set of movies.
+     */
     private void populateMovies() {
         this.movies.put("F001", new Movie("F001", "You've Got Mail", MovieType.REGULAR));
         this.movies.put("F002", new Movie("F002", "Matrix", MovieType.REGULAR));
