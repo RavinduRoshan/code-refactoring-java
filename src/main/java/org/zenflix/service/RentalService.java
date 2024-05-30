3c5f8c2939feff7f5e893d4cd18e51e6d097fd6b
unknown <ravinduroshan.rr@gmail.com>
2024-05-30 17:42:39 +0200

    add java doc for rental service

--- src/main/java/org/zenflix/service/RentalService.java
+++ src/main/java/org/zenflix/service/RentalService.java
@@ -2,7 +2,18 @@ package org.zenflix.service;
 
 import org.zenflix.entity.Customer;
 
+/**
+ * The {@code RentalService} interface provides methods for managing customer rentals.
+ * It includes functionality for generating rental summary statements for customers.
+ */
 public interface RentalService {
 
+    /**
+     * Generates a summary statement for the given customer.
+     * The summary includes details such as the total amount owed and the frequent renter points earned.
+     *
+     * @param customer the customer for whom the summary statement is generated
+     * @return the summary statement as a formatted string
+     */
     String getSummaryStatement(Customer customer);
 }
--- src/main/java/org/zenflix/service/impl/RentalServiceImpl.java
+++ src/main/java/org/zenflix/service/impl/RentalServiceImpl.java
@@ -6,26 +6,51 @@ import org.zenflix.entity.Movie;
 import org.zenflix.entity.MovieRental;
 import org.zenflix.entity.RentalSummary;
 import org.zenflix.repository.MovieRepository;
-import org.zenflix.service.RentalStrategy;
 import org.zenflix.service.RentalService;
+import org.zenflix.service.RentalStrategy;
 import org.zenflix.util.RentalStrategyFactory;
 import org.zenflix.util.StringUtils;
 
 import java.util.List;
 
+/**
+ * Implementation of the {@link RentalService} interface.
+ * This class provides services related to customer rentals,
+ * such as generating rental summaries and calculating rental amounts.
+ */
 public class RentalServiceImpl implements RentalService {
     private final MovieRepository movieRepository;
 
+    /**
+     * Constructs a new {@code RentalServiceImpl} with the specified {@link MovieRepository}.
+     *
+     * @param movieRepository the movie repository used to retrieve movie information
+     */
     public RentalServiceImpl(MovieRepository movieRepository) {
         this.movieRepository = movieRepository;
     }
 
+    /**
+     * Generates a summary statement for the given customer.
+     * The summary includes the total amount owed and the frequent renter points earned.
+     *
+     * @param customer the customer for whom the summary statement is generated
+     * @return the summary statement as a formatted string
+     */
     @Override
     public String getSummaryStatement(Customer customer) {
         RentalSummary rentalSummary = getRentalSummary(customer.rentals());
         return StringUtils.buildStatement(customer.name(), rentalSummary);
     }
 
+    /**
+     * Generates a rental summary for the given list of movie rentals.
+     * The summary includes the total amount owed, the frequent renter points earned,
+     * and the titles with corresponding rental amounts.
+     *
+     * @param rentals the list of movie rentals
+     * @return the rental summary
+     */
     private RentalSummary getRentalSummary(List<MovieRental> rentals) {
         double totalAmount = 0;
         int frequentEnterPoints = 0;
@@ -33,12 +58,9 @@ public class RentalServiceImpl implements RentalService {
 
         for (MovieRental rental : rentals) {
             Movie movie = movieRepository.findById(rental.movieId());
-
             double thisAmount = calculateAmount(movie.type(), rental);
             totalAmount += thisAmount;
-
             frequentEnterPoints += getFrequentEnterPoints(movie.type(), rental.days());
-
             titlesWithAmount.append(StringUtils.getTitleWithAmount(movie.title(), thisAmount));
         }
 
@@ -46,10 +68,10 @@ public class RentalServiceImpl implements RentalService {
     }
 
     /**
-     * Calculates the rental amount for a given movie rental.
+     * Calculates the rental amount for a given movie rental based on its type.
      *
-     * @param rental the movie rental
-     * @param movieType the movieType
+     * @param movieType the type of the movie
+     * @param rental    the movie rental
      * @return the rental amount
      */
     private double calculateAmount(MovieType movieType, MovieRental rental) {
@@ -57,6 +79,13 @@ public class RentalServiceImpl implements RentalService {
         return rentalStrategy.calculatePrice(rental.days());
     }
 
+    /**
+     * Generates the frequent renter points for a given movie rental based on its type and rental days.
+     *
+     * @param movieType the type of the movie
+     * @param days      the number of days the movie is rented for
+     * @return the number of frequent renter points earned
+     */
     private int getFrequentEnterPoints(MovieType movieType, int days) {
         RentalStrategy rentalStrategy = RentalStrategyFactory.getRentalStrategy(movieType);
         return rentalStrategy.generatePoints(days);
